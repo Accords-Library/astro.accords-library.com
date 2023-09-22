481c229478fa6518b234995937e7fba6279b99f0
DrMint <29893320+DrMint@users.noreply.github.com>
2023-09-22 23:02:45 +0200

    Added a progression bar while loading new page

--- src/utils/turbo.ts
+++ src/utils/turbo.ts
@@ -1,2 +1,14 @@
 export const onLoad = (callback: () => void) =>
   document.documentElement.addEventListener("turbo:load", callback);
+
+export const onRequestStart = (callback: () => void) =>
+  document.documentElement.addEventListener(
+    "turbo:before-fetch-request",
+    callback
+  );
+
+export const onRequestEnd = (callback: () => void) =>
+  document.documentElement.addEventListener(
+    "turbo:render",
+    callback
+  );
